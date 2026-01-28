9e86514e77ba063df6eb8135608d859d3d075877
Devadarsh4 <devadarsh7070@gmail.com>
2026-01-28 10:36:14 +0530

    commit

--- src/pages/Student.jsx
+++ src/pages/Student.jsx
@@ -1,7 +1,13 @@
 function Student(){
+    let name = "tommy";
+    let roll number = 10;
     return (
         <>
-        
+        <p>
+            Student Name: {name}
+            <br/>
+            roll number: {roll number}
+        </p>
         </>
     );
 }
