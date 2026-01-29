4239292ee7269a1ff41cf06489fcc546e51d6d77
Devadarsh4 <devadarsh7070@gmail.com>
2026-01-29 09:29:36 +0530

    commit

---
+++ src/pages/practice/App.jsx
@@ -0,0 +1,21 @@
+import Container from "react-bootstrap/Container";
+
+function App(){
+    return (
+        <>
+        <div className="container">
+            <h1 className="text-center"> Welcome to Expense App</h1>
+        </div>
+        <Container>
+            <h1 className="text-center"> Welcome to Expense App</h1>
+        </Container>
+        
+        </>
+    )
+}
+import default App;
+
+
+
+
+
