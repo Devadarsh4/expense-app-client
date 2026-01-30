1ef6cfc2fa988796b09c8b5049e05c357d00799e
Devadarsh4 <devadarsh7070@gmail.com>
2026-01-30 10:08:31 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import AppLayout from "./components/AppLayout";
+
 function App() {
-    return (
-        <Routes>
-            <Route path="/" element={
-                <AppLayout>
-                    <Home />
-                </AppLayout>
-            }/>
-            <Route path="/login" element={
-                <AppLayout>
-                    <Login />
-                </AppLayout>
-            }/>
-        </Routes>
-    );
+  const [userDetails, setUserDetails] = useState(null);
+
+  return (
+    <Routes>
+      <Route element={<AppLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/login"
+          element={<Login setUser={setUserDetails} />}
+        />
+      </Route>
+    </Routes>
+  );
 }
-    
+
 export default App;
