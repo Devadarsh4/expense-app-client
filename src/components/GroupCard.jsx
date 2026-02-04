e5ce01aeb655c8fce6f5f19c497ff5467a59e79b
Devadarsh4 <devadarsh7070@gmail.com>
2026-02-04 09:24:12 +0530

    group-carx

--- src/components/GroupCard.jsx
+++ src/components/GroupCard.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
+
 function GroupCard({ group}) {
+    const [showMenbers,setShowMembers]=useState(false);
 <div className="card h-100 border-0 shadow-sm rounded-4 position-relative">
     <div className="card-body p-4">
         <div>
             <h5 className="">{group.name}</h5>
-            <button>
-                Show Members
+            <button className="btn-sm btn-link p-0 text-primary">
+
+                {group.membersEmail.length} Member | Show Members
             </button>
         </div>
     </div>
