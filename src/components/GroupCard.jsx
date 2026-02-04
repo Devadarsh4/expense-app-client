18e8458051b764395aa11c2de865b6fa35948222
Devadarsh4 <devadarsh7070@gmail.com>
2026-02-04 09:19:44 +0530

    group-carx

--- src/components/GroupCard.jsx
+++ src/components/GroupCard.jsx
@@ -1,7 +1,12 @@
 function GroupCard({ group}) {
 <div className="card h-100 border-0 shadow-sm rounded-4 position-relative">
     <div className="card-body p-4">
-        
+        <div>
+            <h5 className="">{group.name}</h5>
+            <button>
+                Show Members
+            </button>
+        </div>
     </div>
 </div>
 }
