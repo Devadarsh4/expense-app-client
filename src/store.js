e66049b035ae9e55b84c2e3d991aa6f07c8ccdf3
Devadarsh4 <devadarsh7070@gmail.com>
2026-02-07 23:00:10 +0530

    Store.js

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { userReducer } from "./redux/user/reducers";
+import userReducer from "./redux/user/reducers.js";
 
 export const store = configureStore({
     reducer: {
